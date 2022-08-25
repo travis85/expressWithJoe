cb0f6b9f848815a389b4647bbcf8c6f7701dcfeb
travis flake <tflake83@gmail.com>
2022-08-25 09:54:40 -0400

    latest changes for query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios');
 const app = express()
 const port = 3000
 const fs = require('fs');
@@ -7,6 +8,7 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -16,20 +18,29 @@ function logger(req, res, next) {
 }
 app.use(logger)
 
-// function getName(name) {
-//   const name = document.getElementsById('name').val
-//   return name
-// }
 
 app.get('/', async function (req, res) {
-  let names = ['Trav', 'Joe', 'another', 'another guy']
+  const header = 'what up stranger';
+  const users = ['Trav', 'Joe', 'Danny'];
+  const user = req.query.users;
+  
+  console.log(user)
   const viewData = {
-    names: names
+    header: header,
+    user: user
   }
-   await res.render('index', viewData)
+
+  await res.render('index', viewData )
 })
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/**
+ * pass params in url quiries
+ * and use data as render
+ * https://stackabuse.com/get-query-strings-and-parameters-in-express-js/
+
+ */
