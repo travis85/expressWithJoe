5ccf0336527d96f50ef68bb4f852782c31a41292
travis flake <tflake83@gmail.com>
2022-08-29 12:07:54 -0400

    bash command

---
+++ scripts/practice.bash
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# 'shabang' after goes location of program that reads code below
+# when making a bash script: 
+# 1. chmod +x <filename> to make it executable.
+# 2. to check file permissions: "ls -al"
+# 3. to find the location of a program type "which <program>"
+# 4. echo is the same as console.log() for bash
+
+cd /Users/travis/express_with_joe/scripts
+echo $(pwd)
